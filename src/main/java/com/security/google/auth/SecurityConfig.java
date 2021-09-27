d0c6774feae37f06ce20fe7a31a0d05c657126cb
Akshay Usnale <akshayusnale1997@gmail.com>
2021-09-27 14:55:21 +0530

    security configurations done

---
+++ src/main/java/com/security/google/auth/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.security.google.auth;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().oauth2Client();
+    }
+}
--- src/main/java/com/security/google/auth/SpringSsoGoogleApplication.java
+++ src/main/java/com/security/google/auth/SpringSsoGoogleApplication.java
@@ -2,9 +2,26 @@ package com.security.google.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class SpringSsoGoogleApplication {
+
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Google !!";
+	}
+
+	@GetMapping("/users")
+	public Principal user(Principal principal){
+		System.out.println("username" + principal.getName());
+		return principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSsoGoogleApplication.class, args);
