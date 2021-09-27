934f972b3d06bfa328b8539b9b51719fbda5eaa2
Akshay Usnale <akshayusnale1997@gmail.com>
2021-09-27 14:04:19 +0530

    Initial commit

---
+++ src/main/java/com/security/google/auth/SpringSsoGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.security.google.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSsoGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSsoGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/google/auth/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.google.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
